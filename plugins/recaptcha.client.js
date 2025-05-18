8a6442f922ba812e0d8f9cc34e993bc9bb85b320
aaronthomclear <hampeaaron@gmail.com>
2025-05-18 17:54:59 +0200

    changes

--- plugins/recaptcha.client.js
+++ plugins/recaptcha.client.js
@@ -1,9 +1,11 @@
 import { VueReCaptcha } from 'vue-recaptcha-v3'
-
 export default defineNuxtPlugin(nuxtApp => {
   const cfg = useRuntimeConfig()
   nuxtApp.vueApp.use(VueReCaptcha, {
     siteKey: cfg.public.recaptchaSiteKey,
-    loaderOptions: { autoHideBadge: true, useRecaptchaNet: true }
+    loaderOptions: {
+      useRecaptchaNet: true,     // schneller CDN
+      autoHideBadge: false       // Badge sichtbar lassen
+    }
   })
 })
